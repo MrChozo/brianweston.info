826ad2af6e0667e1e266d742736cfb324d985c4c
= <=>
2016-10-27 01:07:27 +0000

    Initial commit

---
+++ resources/views/elements/navigation.blade.php
@@ -0,0 +1,5 @@
+<div class="top-right links foreground">
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/resume">Resume</a>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Welcome!
+            </div>
+            <h2>Brian Weston's homepage is currently under construction.</h2>
+            <h2>Please check back soon.</h2>
+        </div>
+    </div>
+@stop
