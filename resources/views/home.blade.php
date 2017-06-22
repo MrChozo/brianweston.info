9ef65f69a767f6c34cdf9898f2c550c1e6ba42b1
= <=>
2017-06-22 14:17:25 -0700

    Remove iframe

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,10 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                Welcome!
+                Welcome!!
             </div>
  <!--       <h2>Brian Weston's homepage is currently under construction.</h2>
             <h2>Please check back soon.</h2> -->
-            <iframe width="350" height="180" src="https://w2.countingdownto.com/1670142" frameborder="0"></iframe>
         </div>
     </div>
 @stop
