123b480f079bb20f0a35baf96a2afeb0e516426d
MrChozo <MrChozo@gmail.com>
2017-07-31 16:55:37 -0700

    Update Laravel version and change to brianweston.me

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,17 @@
 @section('title', 'Home')
 
 @section('content')
-    <div class="flex-center position-ref full-height">
+
+<h1>foo</h1>
+
+
+{{--     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
                 Welcome!!
             </div>
- <!--       <h2>Brian Weston's homepage is currently under construction.</h2>
-            <h2>Please check back soon.</h2> -->
         </div>
-    </div>
+    </div> --}}
 @stop
 
 
